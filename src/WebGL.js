fce2d2b5390609fc43f2d32084f4b9f12a6e864e
Laca <laszlo.oravecz@gmail.com>
2017-04-13 09:43:21 +0200

    Javascript source

---
+++ src/WebGL.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Laca on 2017. 04. 12..
+ */
+"use strict";
+
+var gGL = null;
+
+/**
+ * 
+ */
+function initializeGL() {
+    var canvas = document.getElementById("GLCanvas");
+    gGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    if (gGL !== null) {
+        gGL.clearColor(0.0, 0.8, 0.0, 1.0);
+    }else{
+        document.write("<br><b>WebGL is not supported</b>");
+    }
+}
+
+/**
+ *
+ */
+function clearCanvas() {
+    gGL.clear(gGL.COLOR_BUFFER_BIT);
+}
+
+/**
+ *
+ */
+function doGLDraw() {
+    initializeGL();
+    clearCanvas();
+}
